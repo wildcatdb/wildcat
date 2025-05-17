04321a617d08d558fba9a246589a4af3df800d36
Alex Gaetano Padula <me@alexpadula.com>
2025-05-17 17:32:13 -0400

    basic utils tests

--- utils_test.go
+++ utils_test.go
@@ -16,3 +16,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 package orindb
+
+import "testing"
+
+func TestExtractTimestampFromFilename(t *testing.T) {
+	tests := []struct {
+		filename string
+		expected int64
+	}{
+		{"1234567890.wal", 1234567890},
+		{"9876543210.wal", 9876543210},
+		{"invalid.wal", 0},
+		{"12345.invalid", 12345},
+		{"", 0},
+		{"12345", 0},
+		{".wal", 0},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.filename, func(t *testing.T) {
+			result := extractTimestampFromFilename(tt.filename)
+			if result != tt.expected {
+				t.Errorf("extractTimestampFromFilename(%q) = %d; want %d", tt.filename, result, tt.expected)
+			}
+		})
+	}
+}
