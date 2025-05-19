78ae8b47b932b6844d322972c1bd70c9d869079b
Alex Gaetano Padula <me@alexpadula.com>
2025-05-19 10:15:04 -0400

    more benchmarks for db

--- db_test.go
+++ db_test.go
@@ -24,6 +24,7 @@ import (
 	"os"
 	"sync"
 	"testing"
+	"time"
 )
 
 // Tests opening a brand new instance.  Will setup an initial WAL and memory table and disk levels.
@@ -69,13 +70,592 @@ func TestOpen(t *testing.T) {
 	wg.Wait()
 }
 
-func BenchmarkOrinDB(b *testing.B) {
+func BenchmarkDBBloomFilterPartialSync(b *testing.B) {
+	defer os.RemoveAll("benchdb")
+
+	logChannel := make(chan string, 100)
+	opts := &Options{
+		Directory:    "benchdb",
+		LogChannel:   logChannel,
+		BloomFilter:  true,
+		SyncOption:   SyncPartial,
+		SyncInterval: time.Nanosecond * 24,
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for msg := range logChannel {
+			b.Logf("Log: %s", msg)
+		}
+	}()
+
+	db, err := Open(opts)
+	if err != nil {
+		b.Fatalf("Failed to open database: %v", err)
+	}
+	defer func() {
+		db.Close()
+		wg.Wait()
+	}()
+
+	const ops = 100_000
+	const valueSize = 256
+
+	b.Log("Pre-filling keys...")
+	for i := 0; i < ops; i++ {
+		err := db.Update(func(txn *Txn) error {
+			key := []byte(fmt.Sprintf("key%d", i))
+			val := make([]byte, valueSize)
+			rand.Read(val)
+			return txn.Put(key, val)
+		})
+		if err != nil {
+			b.Fatalf("Pre-fill failed at key %d: %v", i, err)
+		}
+	}
+
+	b.Run("Write", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			err := db.Update(func(txn *Txn) error {
+				key := []byte(fmt.Sprintf("key%d", rand.Intn(ops)))
+				val := make([]byte, valueSize)
+				rand.Read(val)
+				return txn.Put(key, val)
+			})
+			if err != nil {
+				b.Fatalf("Write failed: %v", err)
+			}
+		}
+	})
+
+	b.Run("Read", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			err := db.Update(func(txn *Txn) error {
+				key := []byte(fmt.Sprintf("key%d", rand.Intn(ops)))
+				_, err := txn.Get(key)
+				return err
+			})
+			if err != nil {
+				b.Fatalf("Read failed: %v", err)
+			}
+		}
+	})
+
+	b.Run("ConcurrentWrite", func(b *testing.B) {
+		b.ResetTimer()
+		var wg sync.WaitGroup
+		wg.Add(b.N)
+		for i := 0; i < b.N; i++ {
+			go func(i int) {
+				defer wg.Done()
+				err := db.Update(func(txn *Txn) error {
+					key := []byte(fmt.Sprintf("concurrent_key%d", rand.Intn(ops)))
+					val := make([]byte, valueSize)
+					rand.Read(val)
+					return txn.Put(key, val)
+				})
+				if err != nil {
+					b.Errorf("Concurrent write failed: %v", err)
+				}
+			}(i)
+		}
+		wg.Wait()
+	})
+
+	b.Run("ConcurrentRead", func(b *testing.B) {
+		b.ResetTimer()
+		var wg sync.WaitGroup
+		wg.Add(b.N)
+		for i := 0; i < b.N; i++ {
+			go func(i int) {
+				defer wg.Done()
+				err := db.Update(func(txn *Txn) error {
+					key := []byte(fmt.Sprintf("key%d", rand.Intn(ops)))
+					_, err := txn.Get(key)
+					return err
+				})
+				if err != nil {
+					b.Errorf("Concurrent read failed: %v", err)
+				}
+			}(i)
+		}
+		wg.Wait()
+	})
+}
+
+func BenchmarkDBPartialSync(b *testing.B) {
+	defer os.RemoveAll("benchdb")
+
+	logChannel := make(chan string, 100)
+	opts := &Options{
+		Directory:    "benchdb",
+		LogChannel:   logChannel,
+		SyncOption:   SyncPartial,
+		SyncInterval: time.Nanosecond * 24,
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for msg := range logChannel {
+			b.Logf("Log: %s", msg)
+		}
+	}()
+
+	db, err := Open(opts)
+	if err != nil {
+		b.Fatalf("Failed to open database: %v", err)
+	}
+	defer func() {
+		db.Close()
+		wg.Wait()
+	}()
+
+	const ops = 100_000
+	const valueSize = 256
+
+	b.Log("Pre-filling keys...")
+	for i := 0; i < ops; i++ {
+		err := db.Update(func(txn *Txn) error {
+			key := []byte(fmt.Sprintf("key%d", i))
+			val := make([]byte, valueSize)
+			rand.Read(val)
+			return txn.Put(key, val)
+		})
+		if err != nil {
+			b.Fatalf("Pre-fill failed at key %d: %v", i, err)
+		}
+	}
+
+	b.Run("Write", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			err := db.Update(func(txn *Txn) error {
+				key := []byte(fmt.Sprintf("key%d", rand.Intn(ops)))
+				val := make([]byte, valueSize)
+				rand.Read(val)
+				return txn.Put(key, val)
+			})
+			if err != nil {
+				b.Fatalf("Write failed: %v", err)
+			}
+		}
+	})
+
+	b.Run("Read", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			err := db.Update(func(txn *Txn) error {
+				key := []byte(fmt.Sprintf("key%d", rand.Intn(ops)))
+				_, err := txn.Get(key)
+				return err
+			})
+			if err != nil {
+				b.Fatalf("Read failed: %v", err)
+			}
+		}
+	})
+
+	b.Run("ConcurrentWrite", func(b *testing.B) {
+		b.ResetTimer()
+		var wg sync.WaitGroup
+		wg.Add(b.N)
+		for i := 0; i < b.N; i++ {
+			go func(i int) {
+				defer wg.Done()
+				err := db.Update(func(txn *Txn) error {
+					key := []byte(fmt.Sprintf("concurrent_key%d", rand.Intn(ops)))
+					val := make([]byte, valueSize)
+					rand.Read(val)
+					return txn.Put(key, val)
+				})
+				if err != nil {
+					b.Errorf("Concurrent write failed: %v", err)
+				}
+			}(i)
+		}
+		wg.Wait()
+	})
+
+	b.Run("ConcurrentRead", func(b *testing.B) {
+		b.ResetTimer()
+		var wg sync.WaitGroup
+		wg.Add(b.N)
+		for i := 0; i < b.N; i++ {
+			go func(i int) {
+				defer wg.Done()
+				err := db.Update(func(txn *Txn) error {
+					key := []byte(fmt.Sprintf("key%d", rand.Intn(ops)))
+					_, err := txn.Get(key)
+					return err
+				})
+				if err != nil {
+					b.Errorf("Concurrent read failed: %v", err)
+				}
+			}(i)
+		}
+		wg.Wait()
+	})
+}
+
+func BenchmarkDBBloomFilter(b *testing.B) {
+	defer os.RemoveAll("benchdb")
+
+	logChannel := make(chan string, 100)
+	opts := &Options{
+		Directory:   "benchdb",
+		LogChannel:  logChannel,
+		BloomFilter: true,
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for msg := range logChannel {
+			b.Logf("Log: %s", msg)
+		}
+	}()
+
+	db, err := Open(opts)
+	if err != nil {
+		b.Fatalf("Failed to open database: %v", err)
+	}
+	defer func() {
+		db.Close()
+		wg.Wait()
+	}()
+
+	const ops = 100_000
+	const valueSize = 256
+
+	b.Log("Pre-filling keys...")
+	for i := 0; i < ops; i++ {
+		err := db.Update(func(txn *Txn) error {
+			key := []byte(fmt.Sprintf("key%d", i))
+			val := make([]byte, valueSize)
+			rand.Read(val)
+			return txn.Put(key, val)
+		})
+		if err != nil {
+			b.Fatalf("Pre-fill failed at key %d: %v", i, err)
+		}
+	}
+
+	b.Run("Write", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			err := db.Update(func(txn *Txn) error {
+				key := []byte(fmt.Sprintf("key%d", rand.Intn(ops)))
+				val := make([]byte, valueSize)
+				rand.Read(val)
+				return txn.Put(key, val)
+			})
+			if err != nil {
+				b.Fatalf("Write failed: %v", err)
+			}
+		}
+	})
+
+	b.Run("Read", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			err := db.Update(func(txn *Txn) error {
+				key := []byte(fmt.Sprintf("key%d", rand.Intn(ops)))
+				_, err := txn.Get(key)
+				return err
+			})
+			if err != nil {
+				b.Fatalf("Read failed: %v", err)
+			}
+		}
+	})
+
+	b.Run("ConcurrentWrite", func(b *testing.B) {
+		b.ResetTimer()
+		var wg sync.WaitGroup
+		wg.Add(b.N)
+		for i := 0; i < b.N; i++ {
+			go func(i int) {
+				defer wg.Done()
+				err := db.Update(func(txn *Txn) error {
+					key := []byte(fmt.Sprintf("concurrent_key%d", rand.Intn(ops)))
+					val := make([]byte, valueSize)
+					rand.Read(val)
+					return txn.Put(key, val)
+				})
+				if err != nil {
+					b.Errorf("Concurrent write failed: %v", err)
+				}
+			}(i)
+		}
+		wg.Wait()
+	})
+
+	b.Run("ConcurrentRead", func(b *testing.B) {
+		b.ResetTimer()
+		var wg sync.WaitGroup
+		wg.Add(b.N)
+		for i := 0; i < b.N; i++ {
+			go func(i int) {
+				defer wg.Done()
+				err := db.Update(func(txn *Txn) error {
+					key := []byte(fmt.Sprintf("key%d", rand.Intn(ops)))
+					_, err := txn.Get(key)
+					return err
+				})
+				if err != nil {
+					b.Errorf("Concurrent read failed: %v", err)
+				}
+			}(i)
+		}
+		wg.Wait()
+	})
+}
+
+func BenchmarkDB(b *testing.B) {
 	defer os.RemoveAll("benchdb")
 
 	logChannel := make(chan string, 100)
 	opts := &Options{
 		Directory:  "benchdb",
 		LogChannel: logChannel,
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for msg := range logChannel {
+			b.Logf("Log: %s", msg)
+		}
+	}()
+
+	db, err := Open(opts)
+	if err != nil {
+		b.Fatalf("Failed to open database: %v", err)
+	}
+	defer func() {
+		db.Close()
+		wg.Wait()
+	}()
+
+	const ops = 100_000
+	const valueSize = 256
+
+	b.Log("Pre-filling keys...")
+	for i := 0; i < ops; i++ {
+		err := db.Update(func(txn *Txn) error {
+			key := []byte(fmt.Sprintf("key%d", i))
+			val := make([]byte, valueSize)
+			rand.Read(val)
+			return txn.Put(key, val)
+		})
+		if err != nil {
+			b.Fatalf("Pre-fill failed at key %d: %v", i, err)
+		}
+	}
+
+	b.Run("Write", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			err := db.Update(func(txn *Txn) error {
+				key := []byte(fmt.Sprintf("key%d", rand.Intn(ops)))
+				val := make([]byte, valueSize)
+				rand.Read(val)
+				return txn.Put(key, val)
+			})
+			if err != nil {
+				b.Fatalf("Write failed: %v", err)
+			}
+		}
+	})
+
+	b.Run("Read", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			err := db.Update(func(txn *Txn) error {
+				key := []byte(fmt.Sprintf("key%d", rand.Intn(ops)))
+				_, err := txn.Get(key)
+				return err
+			})
+			if err != nil {
+				b.Fatalf("Read failed: %v", err)
+			}
+		}
+	})
+
+	b.Run("ConcurrentWrite", func(b *testing.B) {
+		b.ResetTimer()
+		var wg sync.WaitGroup
+		wg.Add(b.N)
+		for i := 0; i < b.N; i++ {
+			go func(i int) {
+				defer wg.Done()
+				err := db.Update(func(txn *Txn) error {
+					key := []byte(fmt.Sprintf("concurrent_key%d", rand.Intn(ops)))
+					val := make([]byte, valueSize)
+					rand.Read(val)
+					return txn.Put(key, val)
+				})
+				if err != nil {
+					b.Errorf("Concurrent write failed: %v", err)
+				}
+			}(i)
+		}
+		wg.Wait()
+	})
+
+	b.Run("ConcurrentRead", func(b *testing.B) {
+		b.ResetTimer()
+		var wg sync.WaitGroup
+		wg.Add(b.N)
+		for i := 0; i < b.N; i++ {
+			go func(i int) {
+				defer wg.Done()
+				err := db.Update(func(txn *Txn) error {
+					key := []byte(fmt.Sprintf("key%d", rand.Intn(ops)))
+					_, err := txn.Get(key)
+					return err
+				})
+				if err != nil {
+					b.Errorf("Concurrent read failed: %v", err)
+				}
+			}(i)
+		}
+		wg.Wait()
+	})
+}
+
+func BenchmarkDBBloomFilterFullSync(b *testing.B) {
+	defer os.RemoveAll("benchdb")
+
+	logChannel := make(chan string, 100)
+	opts := &Options{
+		Directory:   "benchdb",
+		LogChannel:  logChannel,
+		BloomFilter: true,
+		SyncOption:  SyncFull,
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for msg := range logChannel {
+			b.Logf("Log: %s", msg)
+		}
+	}()
+
+	db, err := Open(opts)
+	if err != nil {
+		b.Fatalf("Failed to open database: %v", err)
+	}
+	defer func() {
+		db.Close()
+		wg.Wait()
+	}()
+
+	const ops = 100_000
+	const valueSize = 256
+
+	b.Log("Pre-filling keys...")
+	for i := 0; i < ops; i++ {
+		err := db.Update(func(txn *Txn) error {
+			key := []byte(fmt.Sprintf("key%d", i))
+			val := make([]byte, valueSize)
+			rand.Read(val)
+			return txn.Put(key, val)
+		})
+		if err != nil {
+			b.Fatalf("Pre-fill failed at key %d: %v", i, err)
+		}
+	}
+
+	b.Run("Write", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			err := db.Update(func(txn *Txn) error {
+				key := []byte(fmt.Sprintf("key%d", rand.Intn(ops)))
+				val := make([]byte, valueSize)
+				rand.Read(val)
+				return txn.Put(key, val)
+			})
+			if err != nil {
+				b.Fatalf("Write failed: %v", err)
+			}
+		}
+	})
+
+	b.Run("Read", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			err := db.Update(func(txn *Txn) error {
+				key := []byte(fmt.Sprintf("key%d", rand.Intn(ops)))
+				_, err := txn.Get(key)
+				return err
+			})
+			if err != nil {
+				b.Fatalf("Read failed: %v", err)
+			}
+		}
+	})
+
+	b.Run("ConcurrentWrite", func(b *testing.B) {
+		b.ResetTimer()
+		var wg sync.WaitGroup
+		wg.Add(b.N)
+		for i := 0; i < b.N; i++ {
+			go func(i int) {
+				defer wg.Done()
+				err := db.Update(func(txn *Txn) error {
+					key := []byte(fmt.Sprintf("concurrent_key%d", rand.Intn(ops)))
+					val := make([]byte, valueSize)
+					rand.Read(val)
+					return txn.Put(key, val)
+				})
+				if err != nil {
+					b.Errorf("Concurrent write failed: %v", err)
+				}
+			}(i)
+		}
+		wg.Wait()
+	})
+
+	b.Run("ConcurrentRead", func(b *testing.B) {
+		b.ResetTimer()
+		var wg sync.WaitGroup
+		wg.Add(b.N)
+		for i := 0; i < b.N; i++ {
+			go func(i int) {
+				defer wg.Done()
+				err := db.Update(func(txn *Txn) error {
+					key := []byte(fmt.Sprintf("key%d", rand.Intn(ops)))
+					_, err := txn.Get(key)
+					return err
+				})
+				if err != nil {
+					b.Errorf("Concurrent read failed: %v", err)
+				}
+			}(i)
+		}
+		wg.Wait()
+	})
+}
+
+func BenchmarkDBSyncFull(b *testing.B) {
+	defer os.RemoveAll("benchdb")
+
+	logChannel := make(chan string, 100)
+	opts := &Options{
+		Directory:  "benchdb",
+		LogChannel: logChannel,
+		SyncOption: SyncFull,
 	}
 
 	wg := &sync.WaitGroup{}
