3be4ede01ac1f2632d09e1c3459d94efbb45f582
Alex Gaetano Padula <me@alexpadula.com>
2025-06-02 21:58:21 -0400

    windows ci corrections attempt

--- blockmanager/pio_windows.go
+++ blockmanager/pio_windows.go
@@ -5,24 +5,58 @@ package blockmanager
 
 import "syscall"
 
+var (
+	kernel32           = syscall.NewLazyDLL("kernel32.dll")
+	procSetFilePointer = kernel32.NewProc("SetFilePointer")
+	procReadFile       = kernel32.NewProc("ReadFile")
+	procWriteFile      = kernel32.NewProc("WriteFile")
+)
+
 // pwrite performs an atomic write at a specific offset without needing to Seek first
 func pwrite(fd uintptr, data []byte, offset int64) (int, error) {
-	var overlapped syscall.Overlapped
-	overlapped.OffsetHigh = uint32(offset >> 32)
-	overlapped.Offset = uint32(offset)
+	// Use SetFilePointer + WriteFile as Windows doesn't have native pwrite
+	handle := syscall.Handle(fd)
 
+	// Set file pointer to desired offset
+	low := uint32(offset)
+	high := uint32(offset >> 32)
+
+	_, _, err := procSetFilePointer.Call(
+		uintptr(handle),
+		uintptr(low),
+		uintptr(unsafe.Pointer(&high)),
+		0, // FILE_BEGIN
+	)
+	if err != nil && err.(syscall.Errno) != 0 {
+		return 0, err
+	}
+
+	// Perform the write
 	var bytesWritten uint32
-	err := syscall.WriteFile(syscall.Handle(fd), data, &bytesWritten, &overlapped)
-	return int(bytesWritten), err
+	err2 := syscall.WriteFile(handle, data, &bytesWritten, nil)
+	return int(bytesWritten), err2
 }
 
 // pread performs an atomic read from a specific offset without needing to Seek first
 func pread(fd uintptr, data []byte, offset int64) (int, error) {
-	var overlapped syscall.Overlapped
-	overlapped.OffsetHigh = uint32(offset >> 32)
-	overlapped.Offset = uint32(offset)
+	handle := syscall.Handle(fd)
 
+	// Set file pointer to desired offset
+	low := uint32(offset)
+	high := uint32(offset >> 32)
+
+	_, _, err := procSetFilePointer.Call(
+		uintptr(handle),
+		uintptr(low),
+		uintptr(unsafe.Pointer(&high)),
+		0, // FILE_BEGIN
+	)
+	if err != nil && err.(syscall.Errno) != 0 {
+		return 0, err
+	}
+
+	// Perform the read
 	var bytesRead uint32
-	err := syscall.ReadFile(syscall.Handle(fd), data, &bytesRead, &overlapped)
-	return int(bytesRead), err
+	err2 := syscall.ReadFile(handle, data, &bytesRead, nil)
+	return int(bytesRead), err2
 }
