7cb549752c949513f41d54b3da50a9bc5bc071e7
Alex Gaetano Padula <me@alexpadula.com>
2025-06-02 22:10:29 -0400

    let's try to simplify the block manager write for windows only

--- blockmanager/pio.go
+++ blockmanager/pio.go
@@ -2,14 +2,17 @@
 
 package blockmanager
 
-import "syscall"
+import (
+	"os"
+	"syscall"
+)
 
 // pwrite performs an atomic write at a specific offset without needing to Seek first
-func pwrite(fd uintptr, data []byte, offset int64) (int, error) {
+func pwrite(fd uintptr, data []byte, offset int64, f *os.File) (int, error) {
 	return syscall.Pwrite(int(fd), data, offset)
 }
 
 // pread performs an atomic read from a specific offset without needing to Seek first
-func pread(fd uintptr, data []byte, offset int64) (int, error) {
+func pread(fd uintptr, data []byte, offset int64, f *os.File) (int, error) {
 	return syscall.Pread(int(fd), data, offset)
 }
--- blockmanager/pio_windows.go
+++ blockmanager/pio_windows.go
@@ -3,63 +3,12 @@
 
 package blockmanager
 
-import (
-	"syscall"
-	"unsafe"
-)
-
-var (
-	kernel32           = syscall.NewLazyDLL("kernel32.dll")
-	procSetFilePointer = kernel32.NewProc("SetFilePointer")
-	procReadFile       = kernel32.NewProc("ReadFile")
-	procWriteFile      = kernel32.NewProc("WriteFile")
-)
-
 // pwrite performs an atomic write at a specific offset without needing to Seek first
-func pwrite(fd uintptr, data []byte, offset int64) (int, error) {
-	// Use SetFilePointer + WriteFile as Windows doesn't have native pwrite
-	handle := syscall.Handle(fd)
-
-	// Set file pointer to desired offset
-	low := uint32(offset)
-	high := uint32(offset >> 32)
-
-	_, _, err := procSetFilePointer.Call(
-		uintptr(handle),
-		uintptr(low),
-		uintptr(unsafe.Pointer(&high)),
-		0, // FILE_BEGIN
-	)
-	if err != nil && err.(syscall.Errno) != 0 {
-		return 0, err
-	}
-
-	// Perform the write
-	var bytesWritten uint32
-	err2 := syscall.WriteFile(handle, data, &bytesWritten, nil)
-	return int(bytesWritten), err2
+func pwrite(fd uintptr, data []byte, offset int64, f *os.File) (int, error) {
+	return f.WriteAt(data, offset)
 }
 
 // pread performs an atomic read from a specific offset without needing to Seek first
-func pread(fd uintptr, data []byte, offset int64) (int, error) {
-	handle := syscall.Handle(fd)
-
-	// Set file pointer to desired offset
-	low := uint32(offset)
-	high := uint32(offset >> 32)
-
-	_, _, err := procSetFilePointer.Call(
-		uintptr(handle),
-		uintptr(low),
-		uintptr(unsafe.Pointer(&high)),
-		0, // FILE_BEGIN
-	)
-	if err != nil && err.(syscall.Errno) != 0 {
-		return 0, err
-	}
-
-	// Perform the read
-	var bytesRead uint32
-	err2 := syscall.ReadFile(handle, data, &bytesRead, nil)
-	return int(bytesRead), err2
+func pread(fd uintptr, data []byte, offset int64, f *os.File) (int, error) {
+	return f.ReadAt(data, offset)
 }
